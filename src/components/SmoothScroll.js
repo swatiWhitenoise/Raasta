8ce830a1ae8d2bc5d0a96a867ed6440f9b8f2b71
swatiWhitenoise <59361581+swatiWhitenoise@users.noreply.github.com>
2021-05-26 17:38:29 +0530

    Add files via upload

--- src/components/SmoothScroll.js
+++ src/components/SmoothScroll.js
@@ -1,90 +1,92 @@
-import React, { Component } from "react";
+import React, { useRef, useEffect } from "react";
 import $ from 'jquery'; 
-import { TweenLite } from "gsap/dist/gsap";
-class SmoothScroll extends Component {
-    constructor(props) {
-      super(props);
-    }
-    render() {
-                   // console.log( "ready!" );
+import { TweenLite } from "gsap";
+
+
+function SmoothScroll() {
+  let smoothScrol = useRef(null);
+
+  useEffect(() =>{
+
+    // console.log( "ready!" );
     if($(window).width() >= 600){
-        // do your stuff
-        var html = document.documentElement;
-            var body = document.body;
-      
-            var scroller = {
-              target: document.querySelector("#scroll-container"),
-              ease: 0.05, // <= scroll speed
-              endY: 0,
-              y: 0,
-              resizeRequest: 1,
-              scrollRequest: 0,
-            };
-      
-            var requestId = null;
-      
+      // do your stuff
+      var html = document.documentElement;
+          var body = document.body;
+    
+          var scroller = {
+            target: document.querySelector("#scroll-container"),
+            ease: 0.05, // <= scroll speed
+            endY: 0,
+            y: 0,
+            resizeRequest: 1,
+            scrollRequest: 0,
+          };
+    
+          var requestId = null;
+    
+          TweenLite.set(scroller.target, {
+            rotation: 0.01,
+            force3D: true,
+          });
+    
+          window.addEventListener("load", onLoad);
+    
+          function onLoad() {
+            updateScroller();
+            window.focus();
+            window.addEventListener("resize", onResize);
+            document.addEventListener("scroll", onScroll);
+          }
+    
+          function updateScroller() {
+            var resized = scroller.resizeRequest > 0;
+    
+            if (resized) {
+              var height = scroller.target.clientHeight;
+              body.style.height = height + "px";
+              scroller.resizeRequest = 0;
+            }
+    
+            var scrollY =
+              window.pageYOffset || html.scrollTop || body.scrollTop || 0;
+    
+            scroller.endY = scrollY;
+            scroller.y += (scrollY - scroller.y) * scroller.ease;
+    
+            if (Math.abs(scrollY - scroller.y) < 0.05 || resized) {
+              scroller.y = scrollY;
+              scroller.scrollRequest = 0;
+            }
+    
             TweenLite.set(scroller.target, {
-              rotation: 0.01,
-              force3D: true,
+              y: -scroller.y,
             });
-      
-            window.addEventListener("load", onLoad);
-      
-            function onLoad() {
-              updateScroller();
-              window.focus();
-              window.addEventListener("resize", onResize);
-              document.addEventListener("scroll", onScroll);
+    
+            requestId =
+              scroller.scrollRequest > 0
+                ? requestAnimationFrame(updateScroller)
+                : null;
+          }
+    
+          function onScroll() {
+            scroller.scrollRequest++;
+            if (!requestId) {
+              requestId = requestAnimationFrame(updateScroller);
             }
-      
-            function updateScroller() {
-              var resized = scroller.resizeRequest > 0;
-      
-              if (resized) {
-                var height = scroller.target.clientHeight;
-                body.style.height = height + "px";
-                scroller.resizeRequest = 0;
-              }
-      
-              var scrollY =
-                window.pageYOffset || html.scrollTop || body.scrollTop || 0;
-      
-              scroller.endY = scrollY;
-              scroller.y += (scrollY - scroller.y) * scroller.ease;
-      
-              if (Math.abs(scrollY - scroller.y) < 0.05 || resized) {
-                scroller.y = scrollY;
-                scroller.scrollRequest = 0;
-              }
-      
-              TweenLite.set(scroller.target, {
-                y: -scroller.y,
-              });
-      
-              requestId =
-                scroller.scrollRequest > 0
-                  ? requestAnimationFrame(updateScroller)
-                  : null;
+          }
+    
+          function onResize() {
+            scroller.resizeRequest++;
+            if (!requestId) {
+              requestId = requestAnimationFrame(updateScroller);
             }
-      
-            function onScroll() {
-              scroller.scrollRequest++;
-              if (!requestId) {
-                requestId = requestAnimationFrame(updateScroller);
-              }
-            }
-      
-            function onResize() {
-              scroller.resizeRequest++;
-              if (!requestId) {
-                requestId = requestAnimationFrame(updateScroller);
-              }
-            }
-      }
-         
-      return(
-          <div></div>
-      );
+          }
     }
+  });
+  return(
+    <div id="scroll-container" ref={el => smoothScrol = el}></div>
+);
 }
+
 export default SmoothScroll
